8c286ce349fda1da136278cf582f8ac2cadeab15
Abhinav Chaudhary <abhinav.chaudhary@happiestminds.com>
2022-12-26 18:48:35 +0530

    Added trxn-dashboard

--- src/app/transaction/transaction.module.ts
+++ src/app/transaction/transaction.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 import { MakeTransactionComponent } from './make-transaction/make-transaction.component';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { TrxnDashboardComponent } from './trxn-dashboard/trxn-dashboard.component';
 
 
 
 @NgModule({
   declarations: [
-    MakeTransactionComponent
+    MakeTransactionComponent,
+    TrxnDashboardComponent
   ],
   imports: [
     CommonModule,
@@ -16,7 +18,8 @@ import { HttpClientModule } from '@angular/common/http';
     HttpClientModule
   ],
   exports: [
-    MakeTransactionComponent
+    MakeTransactionComponent,
+    TrxnDashboardComponent
   ]
 })
 export class TransactionModule { }
---
+++ src/app/transaction/trxn-dashboard/trxn-dashboard.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-trxn-dashboard',
+  templateUrl: './trxn-dashboard.component.html',
+  styleUrls: ['./trxn-dashboard.component.css']
+})
+export class TrxnDashboardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
