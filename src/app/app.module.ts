26c4ebc1d5ce38c0bc1725fe48fc5aa4b76baedb
Lovedeep <lovedeep@happiestminds.com>
2022-12-26 09:38:28 +0530

    added components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LandingComponent } from './pages/landing/landing.component';
+import { LandingNavComponent } from './layout/landing-nav/landing-nav.component';
+import { CrouselComponent } from './components/crousel/crousel.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import { ContactQueryComponent } from './components/query/contact-query/contact-query.component';
+import { UserQueryComponent } from './components/query/user-query/user-query.component';
+import { FormsModule } from '@angular/forms';
+import { UserOffersComponent } from './components/user-offers/user-offers.component';
+import { LoginComponent } from './pages/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingComponent,
+    LandingNavComponent,
+    CrouselComponent,
+    FooterComponent,
+    ContactQueryComponent,
+    UserQueryComponent,
+    UserOffersComponent,
+    LoginComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
