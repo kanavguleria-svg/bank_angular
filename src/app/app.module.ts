7c9b1dfee4b6c7eba592545d1392c0835747b155
Kanan Guleria <kanan.guleria@happiestminds.com>
2022-12-26 10:55:25 +0530

    kanav credit cared ceheckbook

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CreditCardComponent } from './credit-card/credit-card.component';
+import { CheckbookComponent } from './checkbook/checkbook.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreditCardComponent,
+    CheckbookComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/checkbook/checkbook.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+
+@Component({
+  selector: 'app-checkbook',
+  templateUrl: './checkbook.component.html',
+  styleUrls: ['./checkbook.component.css'],
+})
+export class CheckbookComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+    
+  }
+
+}
