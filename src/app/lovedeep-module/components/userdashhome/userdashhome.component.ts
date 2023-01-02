8436fa6133b3cd9f38d7a5805e4f2c4a1fc3659b
Abhinav Chaudhary <abhinav.chaudhary@happiestminds.com>
2023-01-02 13:30:40 +0530

    merge main

--- src/app/lovedeep-module/components/userdashhome/userdashhome.component.ts
+++ src/app/lovedeep-module/components/userdashhome/userdashhome.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Customer } from 'src/app/models/customer.model';
+import { TrxnService } from 'src/app/transaction/trxn.service';
 
 @Component({
   selector: 'app-userdashhome',
@@ -9,12 +10,18 @@ import { Customer } from 'src/app/models/customer.model';
 export class UserdashhomeComponent implements OnInit {
   customer = new Customer();
   accountBalance: number;
-  constructor() {}
+  constructor(private service: TrxnService) {}
 
   ngOnInit(): void {
     if (sessionStorage.getItem('userdetails')) {
       this.customer = JSON.parse(sessionStorage.getItem('userdetails')!);
       this.accountBalance = this.customer['account_details'].account_balance;
     }
+    this.service.getAccount(this.customer['customer_id']).subscribe(data => {
+      this.accountBalance = data.account_balance;
+    }, error => {
+      console.log(error);
+    }
+    )
   }
 }
